46a3a19d2702ed658cee47edafcf9b0227d86432
Irmen de Jong <irmen@razorvine.net>
2020-03-03 00:07:43 +0100

    musashi

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    implementation("net.java.dev.jna:jna:5.5.0")
+
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
