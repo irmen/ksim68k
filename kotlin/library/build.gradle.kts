b65902fc729e209bb2efcc652077f5c04ff58d5c
Irmen de Jong <irmen@razorvine.net>
2020-10-23 00:01:06 +0200

    update gradle

--- kotlin/library/build.gradle.kts
+++ kotlin/library/build.gradle.kts
@@ -6,7 +6,7 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.70"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
